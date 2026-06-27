4c3b3344e6a3f7753557c4803c953c44ebafcc24
AhmedTheMagnificent <ahmedmanooo14102005@gmail.com>
2026-06-27 09:16:55 +0500

    Somethings

--- sphere.h
+++ sphere.h
@@ -7,6 +7,7 @@
 class sphere : public hittable{
     public:
         sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
+
         bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override{
             vec3 oc = center - r.origin();
             auto a = r.direction().length_squared();
@@ -29,7 +30,8 @@ class sphere : public hittable{
 
             rec.t = root;
             rec.p = r.at(rec.t);
-            rec.normal = (rec.p - center) / radius;
+            vec3 outward_normal = (rec.p - center) / radius;
+            rec.set_face_normal(r, outward_normal);
 
             return true;
         }
